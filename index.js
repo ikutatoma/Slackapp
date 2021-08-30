532c94eb329a6b2e21dd78ec9a8a89d11e1df4ed
ikutatoma <tomaikuta171013@gmail.comm>
2021-08-30 18:21:56 +0900

    [fix] Form Checkを廃止し、文字のみでの確認メッセージとする。入力ミスがある場合はdeleteコマンドを実行してもらうと考えている

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const book = require('./components/book')
+const book = require('./components/book');
+const checkFormBox = require('./components/checkFormBox')
 const {App} = require('@slack/bolt');
 const { WebClient } = require('@slack/web-api');
 
@@ -13,6 +14,8 @@ const app = new App({
     signingSecret:signingSecret,
 });
 
+//message集
+//最初に見せるメッセージ
 const firstMessage = async() =>{
     const client = new WebClient(token);
     const params = {
@@ -21,7 +24,21 @@ const firstMessage = async() =>{
     }; 
     await client.chat.postMessage(params);
 }
-//command 
+//bookコマンド 送信後のメッセージ
+const checkMessage = async(place_data,date_data,start_data,finish_data,user_name) =>{
+    const user = "@" + user_name
+    const prefaceText = "以下の内容で送信しました。入力に間違いがないか確認してください！\nまだプロトタイプなので申し訳ありませんが、入力間違いがありましたら/deleteと打って削除をお願いします(/deleteすら未実装)...\n";
+    const contentText = "日時 : " + date_data + "\n場所 : " + place_data + "\n開始時間 : " + start_data + "\n終了時間 : " + finish_data;
+    checkFormBox.blocks[0].text.text = prefaceText + contentText;
+    const client = new WebClient(token);
+    var params = {
+      channel: user,
+      text:prefaceText + contentText
+    };
+    await client.chat.postMessage(params);
+}
+
+//command集
 app.command('/book', async ({ack,payload,client}) => {
     await ack();
     await client.views.open({
@@ -29,6 +46,8 @@ app.command('/book', async ({ack,payload,client}) => {
         view:book
     });
 });
+
+//action集
 // bookコマンドの際の各sectionの反応
 app.action('add_place', async ({ack,respond,action}) => {
     await ack();
@@ -47,11 +66,15 @@ app.action('add_start_time', async ({ack,respond,action}) => {
 app.action('add_finish_time', async ({ack,respond,action}) => {
     await ack(); 
 });
+//checkFormBoxのaciton
+app.action('checkFormBox', async ({ack,respond,action}) => {
+    await ack(); 
+    //DBに追加する。
+    respond("送信完了しました!");
+});
 //bookコマンド modal submit時
-
 app.view('modal_view', async ({ ack, body, view, client }) => {
     await ack();
-
     var place_data = view["state"]["values"]["block_place"].add_place.selected_option.text.text;
     var date_data = view["state"]["values"]["block_date"].add_date.selected_date;
     var start_data = view["state"]["values"]["block_start_time"].add_start_time.selected_option.text.text;
@@ -61,17 +84,6 @@ app.view('modal_view', async ({ ack, body, view, client }) => {
     checkMessage(place_data,date_data,start_data,finish_data,body.user.name);
 });
 
-const checkMessage = async(place_data,date_data,start_data,finish_data,user_name) =>{
-    const user = "@" + user_name
-    const prefaceText = "入力に間違いがないか確認してください！\nまだプロトタイプなので申し訳ありませんが、入力間違いがありましたらもう一度/bookと打ってください...\n";
-    const contentText = "日時 : " + date_data + "\n場所 : " + place_data + "\n開始時間 : " + start_data + "\n終了時間 : " + finish_data;
-    const client = new WebClient(token);
-    const params = {
-      channel: user,
-      text: prefaceText + contentText
-    }; 
-    await client.chat.postMessage(params);
-}
 
 
 (async () => {
