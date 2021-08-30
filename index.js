2bca1add69ff2a905c8cc6e912d2e28349b6434a
ikutatoma <tomaikuta171013@gmail.comm>
2021-08-30 18:25:50 +0900

    [fix] 文字のみでの確認メッセージになったので、checkFormBox.jsonを廃止した

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const book = require('./components/book');
-const checkFormBox = require('./components/checkFormBox')
 const {App} = require('@slack/bolt');
 const { WebClient } = require('@slack/web-api');
 
@@ -29,7 +28,6 @@ const checkMessage = async(place_data,date_data,start_data,finish_data,user_name
     const user = "@" + user_name
     const prefaceText = "以下の内容で送信しました。入力に間違いがないか確認してください！\nまだプロトタイプなので申し訳ありませんが、入力間違いがありましたら/deleteと打って削除をお願いします(/deleteすら未実装)...\n";
     const contentText = "日時 : " + date_data + "\n場所 : " + place_data + "\n開始時間 : " + start_data + "\n終了時間 : " + finish_data;
-    checkFormBox.blocks[0].text.text = prefaceText + contentText;
     const client = new WebClient(token);
     var params = {
       channel: user,
@@ -66,12 +64,7 @@ app.action('add_start_time', async ({ack,respond,action}) => {
 app.action('add_finish_time', async ({ack,respond,action}) => {
     await ack(); 
 });
-//checkFormBoxのaciton
-app.action('checkFormBox', async ({ack,respond,action}) => {
-    await ack(); 
-    //DBに追加する。
-    respond("送信完了しました!");
-});
+
 //bookコマンド modal submit時
 app.view('modal_view', async ({ ack, body, view, client }) => {
     await ack();
