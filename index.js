20d847ee50f7c1701c59c81ddbe39a902208af0d
ikutatoma <tomaikuta171013@gmail.comm>
2021-08-31 20:45:42 +0900

    [feat] 最初のメッセージの追加。

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { WebClient } = require('@slack/web-api');
 
 let memo = [];
 let today = new Date();
-book.blocks[1].element.initial_date = today.getFullYear() + "-" +  (today.getMonth() + 1) + "-"+ today.getDate();
+book.blocks[2].element.initial_date = today.getFullYear() + "-" +  (today.getMonth() + 1) + "-"+ today.getDate();
 
 const token = "xoxb-2373497621991-2411908742896-iz21znKQCqtRCCEAi9ExhRim";
 const signingSecret = "9a9b592ece65b941f12e58a65130a396";
@@ -20,7 +20,7 @@ const firstMessage = async() =>{
     const client = new WebClient(token);
     const params = {
       channel: 'keymanagement',
-      text: "/bookを実行すれば鍵の管理ができます。"
+      text: "鍵管理システム,プロトタイプです！「こんなことしてほしい！」「ここが使いずらい」などの意見を教えていただけるとすごく励みになります。\n使い方 : /bookを実行すれば鍵の予約ができます。"
     }; 
     await client.chat.postMessage(params);
 }
@@ -29,7 +29,7 @@ const firstMessage = async() =>{
 const checkMessage = async(place_data,date_data,start_data,finish_data,user_name) =>{
     const user = "@" + user_name
     const prefaceText = "以下の内容で送信しました。入力に間違いがないか確認してください！\nまだプロトタイプなので申し訳ありませんが、入力間違いがありましたら/deleteと打って削除をお願いします(/deleteすら未実装)...\n";
-    const contentText = "日時 : " + date_data + "\n場所 : " + place_data + "\n開始時間 : " + start_data + "\n終了時間 : " + finish_data;
+    const contentText = "\n日時 : " + date_data + "\n場所 : " + place_data + "\n開始時間 : " + start_data + "\n終了時間 : " + finish_data;
     const client = new WebClient(token);
     var params = {
       channel: user,
