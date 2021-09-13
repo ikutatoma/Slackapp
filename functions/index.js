218ee03e7e653e3936b1c4f0dcc7f7ebe867678a
ikutatoma <tomaikuta171013@gmail.comm>
2021-09-13 16:21:22 +0900

    [feat]cloud functionsの追加

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.postRequestFunction = functions.https.onRequest((request, response) => {
+  if (request.method !== 'POST') {
+    response.send('This is not post request')
+  }
+
+  // なんか処理
+
+  response.send('This is post request')
+})
